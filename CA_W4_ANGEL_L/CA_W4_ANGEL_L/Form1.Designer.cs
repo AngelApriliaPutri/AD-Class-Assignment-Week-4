8192721bb315d609e81b1dade8132d4bb92f1a28
AngelApriliaPutri <164986194+AngelApriliaPutri@users.noreply.github.com>
2024-03-26 12:54:23 +0700

    Add files via upload

---
+++ CA_W4_ANGEL_L/CA_W4_ANGEL_L/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace CA_W4_ANGEL_L
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_nama = new System.Windows.Forms.Label();
+            this.label_age = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_age = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label_favcolor = new System.Windows.Forms.Label();
+            this.radioButton_blue = new System.Windows.Forms.RadioButton();
+            this.radioButton_yellow = new System.Windows.Forms.RadioButton();
+            this.radioButton_pink = new System.Windows.Forms.RadioButton();
+            this.label_hobi = new System.Windows.Forms.Label();
+            this.checkBox_reading = new System.Windows.Forms.CheckBox();
+            this.checkBox_watchtv = new System.Windows.Forms.CheckBox();
+            this.checkBox_sports = new System.Windows.Forms.CheckBox();
+            this.checkBox_lain = new System.Windows.Forms.CheckBox();
+            this.button_submit = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButton_male = new System.Windows.Forms.RadioButton();
+            this.radioButton_female = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_nama
+            // 
+            this.label_nama.AutoSize = true;
+            this.label_nama.Location = new System.Drawing.Point(22, 20);
+            this.label_nama.Name = "label_nama";
+            this.label_nama.Size = new System.Drawing.Size(51, 20);
+            this.label_nama.TabIndex = 0;
+            this.label_nama.Text = "Name";
+            // 
+            // label_age
+            // 
+            this.label_age.AutoSize = true;
+            this.label_age.Location = new System.Drawing.Point(22, 57);
+            this.label_age.Name = "label_age";
+            this.label_age.Size = new System.Drawing.Size(38, 20);
+            this.label_age.TabIndex = 1;
+            this.label_age.Text = "Age";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Gender";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton_female);
+            this.panel1.Controls.Add(this.radioButton_male);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 80);
+            this.panel1.TabIndex = 3;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(170, 17);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(160, 26);
+            this.textBox_name.TabIndex = 1;
+            // 
+            // textBox_age
+            // 
+            this.textBox_age.Location = new System.Drawing.Point(170, 55);
+            this.textBox_age.Name = "textBox_age";
+            this.textBox_age.Size = new System.Drawing.Size(160, 26);
+            this.textBox_age.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButton_pink);
+            this.panel2.Controls.Add(this.radioButton_yellow);
+            this.panel2.Controls.Add(this.radioButton_blue);
+            this.panel2.Controls.Add(this.label_favcolor);
+            this.panel2.Location = new System.Drawing.Point(12, 173);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(393, 105);
+            this.panel2.TabIndex = 6;
+            // 
+            // label_favcolor
+            // 
+            this.label_favcolor.AutoSize = true;
+            this.label_favcolor.Location = new System.Drawing.Point(10, 11);
+            this.label_favcolor.Name = "label_favcolor";
+            this.label_favcolor.Size = new System.Drawing.Size(125, 20);
+            this.label_favcolor.TabIndex = 0;
+            this.label_favcolor.Text = "Favourite Colour";
+            // 
+            // radioButton_blue
+            // 
+            this.radioButton_blue.AutoSize = true;
+            this.radioButton_blue.Location = new System.Drawing.Point(158, 11);
+            this.radioButton_blue.Name = "radioButton_blue";
+            this.radioButton_blue.Size = new System.Drawing.Size(66, 24);
+            this.radioButton_blue.TabIndex = 1;
+            this.radioButton_blue.TabStop = true;
+            this.radioButton_blue.Text = "Blue";
+            this.radioButton_blue.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_yellow
+            // 
+            this.radioButton_yellow.AutoSize = true;
+            this.radioButton_yellow.Location = new System.Drawing.Point(158, 41);
+            this.radioButton_yellow.Name = "radioButton_yellow";
+            this.radioButton_yellow.Size = new System.Drawing.Size(80, 24);
+            this.radioButton_yellow.TabIndex = 2;
+            this.radioButton_yellow.TabStop = true;
+            this.radioButton_yellow.Text = "Yellow";
+            this.radioButton_yellow.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_pink
+            // 
+            this.radioButton_pink.AutoSize = true;
+            this.radioButton_pink.Location = new System.Drawing.Point(158, 71);
+            this.radioButton_pink.Name = "radioButton_pink";
+            this.radioButton_pink.Size = new System.Drawing.Size(64, 24);
+            this.radioButton_pink.TabIndex = 3;
+            this.radioButton_pink.TabStop = true;
+            this.radioButton_pink.Text = "Pink";
+            this.radioButton_pink.UseVisualStyleBackColor = true;
+            // 
+            // label_hobi
+            // 
+            this.label_hobi.AutoSize = true;
+            this.label_hobi.Location = new System.Drawing.Point(22, 295);
+            this.label_hobi.Name = "label_hobi";
+            this.label_hobi.Size = new System.Drawing.Size(68, 20);
+            this.label_hobi.TabIndex = 7;
+            this.label_hobi.Text = "Hobbies";
+            // 
+            // checkBox_reading
+            // 
+            this.checkBox_reading.AutoSize = true;
+            this.checkBox_reading.Location = new System.Drawing.Point(170, 294);
+            this.checkBox_reading.Name = "checkBox_reading";
+            this.checkBox_reading.Size = new System.Drawing.Size(95, 24);
+            this.checkBox_reading.TabIndex = 8;
+            this.checkBox_reading.Text = "Reading";
+            this.checkBox_reading.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_watchtv
+            // 
+            this.checkBox_watchtv.AutoSize = true;
+            this.checkBox_watchtv.Location = new System.Drawing.Point(170, 324);
+            this.checkBox_watchtv.Name = "checkBox_watchtv";
+            this.checkBox_watchtv.Size = new System.Drawing.Size(126, 24);
+            this.checkBox_watchtv.TabIndex = 9;
+            this.checkBox_watchtv.Text = "Watching TV";
+            this.checkBox_watchtv.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_sports
+            // 
+            this.checkBox_sports.AutoSize = true;
+            this.checkBox_sports.Location = new System.Drawing.Point(170, 354);
+            this.checkBox_sports.Name = "checkBox_sports";
+            this.checkBox_sports.Size = new System.Drawing.Size(136, 24);
+            this.checkBox_sports.TabIndex = 10;
+            this.checkBox_sports.Text = "Playing Sports";
+            this.checkBox_sports.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_lain
+            // 
+            this.checkBox_lain.AutoSize = true;
+            this.checkBox_lain.Location = new System.Drawing.Point(170, 384);
+            this.checkBox_lain.Name = "checkBox_lain";
+            this.checkBox_lain.Size = new System.Drawing.Size(22, 21);
+            this.checkBox_lain.TabIndex = 11;
+            this.checkBox_lain.UseVisualStyleBackColor = true;
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(545, 93);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(96, 47);
+            this.button_submit.TabIndex = 12;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(545, 234);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(96, 44);
+            this.button_clear.TabIndex = 13;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 379);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 26);
+            this.textBox1.TabIndex = 14;
+            // 
+            // radioButton_male
+            // 
+            this.radioButton_male.AutoSize = true;
+            this.radioButton_male.Location = new System.Drawing.Point(162, 11);
+            this.radioButton_male.Name = "radioButton_male";
+            this.radioButton_male.Size = new System.Drawing.Size(68, 24);
+            this.radioButton_male.TabIndex = 3;
+            this.radioButton_male.TabStop = true;
+            this.radioButton_male.Text = "Male";
+            this.radioButton_male.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_female
+            // 
+            this.radioButton_female.AutoSize = true;
+            this.radioButton_female.Location = new System.Drawing.Point(162, 41);
+            this.radioButton_female.Name = "radioButton_female";
+            this.radioButton_female.Size = new System.Drawing.Size(87, 24);
+            this.radioButton_female.TabIndex = 4;
+            this.radioButton_female.TabStop = true;
+            this.radioButton_female.Text = "Female";
+            this.radioButton_female.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_submit);
+            this.Controls.Add(this.checkBox_lain);
+            this.Controls.Add(this.checkBox_sports);
+            this.Controls.Add(this.checkBox_watchtv);
+            this.Controls.Add(this.checkBox_reading);
+            this.Controls.Add(this.label_hobi);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.textBox_age);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label_age);
+            this.Controls.Add(this.label_nama);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_nama;
+        private System.Windows.Forms.Label label_age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_age;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label_favcolor;
+        private System.Windows.Forms.RadioButton radioButton_pink;
+        private System.Windows.Forms.RadioButton radioButton_yellow;
+        private System.Windows.Forms.RadioButton radioButton_blue;
+        private System.Windows.Forms.Label label_hobi;
+        private System.Windows.Forms.CheckBox checkBox_reading;
+        private System.Windows.Forms.CheckBox checkBox_watchtv;
+        private System.Windows.Forms.CheckBox checkBox_sports;
+        private System.Windows.Forms.CheckBox checkBox_lain;
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButton_female;
+        private System.Windows.Forms.RadioButton radioButton_male;
+    }
+}
+
---
+++ CA_W4_ANGEL_L/CA_W4_ANGEL_L/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CA_W4_ANGEL_L
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string Gender;
+        string text = "";
+        int nol = 0;
+        int satu = 0;
+        int dua = 0;
+        bool validsemua = true;
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < textBox_name.Text.Length; i++)
+            {
+                if (Char.IsDigit(textBox_name.Text[i]))
+                {
+                    nol++;
+                }
+            }
+            for (int i = 0; i < textBox_age.Text.Length; i++)
+            {
+                if (Char.IsDigit(textBox_age.Text[i]))
+                {
+                    satu++;
+                }
+                else
+                {
+                    satu = 0;
+                    break;
+                }
+
+            }
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (Char.IsDigit(textBox1.Text[i]))
+                {
+                    dua++;
+                }
+            }
+            if (radioButton_male.Checked)
+            {
+                Gender = "Male";
+            }
+            else
+            {
+                Gender = "Female";
+            }
+            if (radioButton_blue.Checked)
+            {
+                BackColor = Color.LightBlue;
+            }
+            else if (radioButton_yellow.Checked)
+            {
+                BackColor = Color.LightYellow;
+            }
+            else if (radioButton_pink.Checked)
+            {
+                BackColor = Color.Pink;
+            }
+
+            if (checkBox_reading.Checked)
+            {
+                text += checkBox_reading.Text;
+            }
+            if (checkBox_watchtv.Checked)
+            {
+                if (text != "")
+                {
+                    text += ", ";
+                }
+                text += checkBox_watchtv.Text;
+            }
+            if (checkBox_sports.Checked)
+            {
+                if (text != "")
+                {
+                    text += ", ";
+                }
+                text += checkBox_sports.Text;
+            }
+            if (checkBox_lain.Checked)
+            {
+                if (text != "")
+                {
+                    text += ", ";
+                }
+                text += textBox1.Text;
+            }
+            if (nol > 0 || satu == 0 || dua > 0)
+            {
+                MessageBox.Show("Name must not contain number, Age must be numbers, Hobby must not contain number");
+                validsemua = false;
+            }
+            if (validsemua == true)
+            {
+                MessageBox.Show($"Name : {textBox_name.Text} \nAge : {textBox_age.Text} \nGender : {Gender} \nHobbies : {text}");
+            }
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            textBox_name.Clear();
+            textBox_age.Clear();
+            radioButton_male.Checked = false;
+            radioButton_female.Checked = false;
+            radioButton_blue.Checked = false;
+            radioButton_pink.Checked = false;
+            radioButton_yellow.Checked = false;
+            checkBox_reading.Checked = false;
+            checkBox_sports.Checked = false;
+            checkBox_watchtv.Checked = false;
+            checkBox_lain.Checked = false;
+            textBox1.Clear();
+        }
+    }
+}
